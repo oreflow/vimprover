557a2c9c1aff709ce28f482d75b324e3e68c6622
Tim M <oreflow@gmail.com>
2021-02-27 14:47:22 +0100

    Adding keyboard package that monitors Keyboard state and history

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ package main
 import (
   "fmt"
   "oreflow.com/vimprover/listener"
+  "oreflow.com/vimprover/keyboard"
 )
 
 func main() {
   fmt.Printf("Starting vimprover.");
+  state := keyboard.NewKeyboardState()
   err := listener.RunListener(func(ke listener.KeyEvent) {
     fmt.Printf("Got Key Event %+v\n", ke)
+    state = state.AddEvent(ke)
+    fmt.Printf("Current State %+v\n", state)
   })
   if err != nil {
     fmt.Printf("Vimprover terminated with error %+v.", err);
